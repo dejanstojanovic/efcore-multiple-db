ee0fefbdb2440166761e03544e03cb43617cefbe
Dejan Stojanovic <d.stojanovic@marcura.com>
2021-12-29 00:13:31 +0400

    test: checking creadentials flow for SWT

---
+++ MulitpleDb.Sample/Controllers/AuthorizeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MulitpleDb.Sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizeController : ControllerBase
+    {
+        
+    }
+}
